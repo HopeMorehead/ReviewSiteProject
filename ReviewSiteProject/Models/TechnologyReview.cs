d289ae1bf678070a6624df85f00f7c642203c781
HopeMorehead <splendidwonder@gmail.com>
2017-07-28 14:11:34 -0400

    first commit

---
+++ ReviewSiteProject/Models/TechnologyReview.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReviewSiteProject.Models
+{
+    public class TechnologyReview
+    {
+    }
+}
